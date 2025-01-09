1f124d6fda0a7d5dac335c134e573c0f595bb09d
Nick Spinale <nick@nickspinale.com>
2025-01-09 07:57:34 -0800

    Experiment with profiling

---
+++ hs/components/profiling-scratch/Main.hs
@@ -0,0 +1,23 @@
+module Main
+    ( main
+    ) where
+
+import System.Environment
+import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.Builder as B
+
+import BV.ConcreteSyntax
+import BV.Core.Types
+import BV.System.TargetDir
+import BV.System.Test.Utils
+
+main :: IO ()
+main = do
+    path:_ <- getArgs
+    r <- readProofChecks (TargetDir path)
+    case r of
+        Left err -> error err
+        Right x -> do
+            let t = (B.toLazyText . buildProofChecksForManyFile) x
+            print t
+            print $ T.length t `div` 1024
