a8cd1dfd3e8783ed140f4ecba56d0a078c8c60f4
Nick Spinale <nick@nickspinale.com>
2025-01-17 08:10:08 -0800

    x

--- hs/components/core/BV/Core/Graph.hs
+++ hs/components/core/BV/Core/Graph.hs
@@ -2,15 +2,19 @@
 
 module BV.Core.Graph
     ( NodeGraph (..)
+    , makeNodeGraphWith
     , makeNodeGraph
+    , reachable
     ) where
 
-import Data.Graph (Graph, Vertex, graphFromEdges)
+import Data.Graph (Graph, Vertex)
+import qualified Data.Graph as G
 import Data.Map (Map)
 import qualified Data.Map as M
 import qualified GHC.Arr as A
 import GHC.Generics (Generic)
 import Optics.Core
+import Data.Maybe (fromJust)
 
 import BV.Core.Types
 
@@ -22,11 +26,17 @@ data NodeGraph
       }
   deriving (Generic)
 
-makeNodeGraph :: Map NodeAddr Node -> NodeGraph
-makeNodeGraph = makeNodeGraphTheirs
+makeNodeGraphWith :: (a -> Node) -> Map NodeAddr a -> NodeGraph
+makeNodeGraphWith = makeNodeGraphWithTheirs
 
-makeNodeGraphOurs :: Map NodeAddr Node -> NodeGraph
-makeNodeGraphOurs nodeMap =
+makeNodeGraph :: NodeMap -> NodeGraph
+makeNodeGraph = makeNodeGraphWith id
+
+reachable :: NodeGraph -> NodeId -> [NodeId]
+reachable g from = map g.nodeIdMap $ G.reachable g.graph (fromJust (g.nodeIdMapRev from))
+
+makeNodeGraphWithOurs ::  (a -> Node) -> Map NodeAddr a -> NodeGraph
+makeNodeGraphWithOurs f nodeMap =
     NodeGraph
         { graph
         , nodeIdMap = (nodeIdMap' A.!)
@@ -36,7 +46,7 @@ makeNodeGraphOurs nodeMap =
     vertices = zip [0 ..]
         ( (Ret, [])
         : (Err, [])
-        : (M.assocs nodeMap <&> (_1 %~ Addr) . (_2 %~ (^.. nodeConts % to (nodeIdMapRev' M.!))))
+        : (M.assocs nodeMap <&> (_1 %~ Addr) . (_2 %~ (^.. to f % nodeConts % to (nodeIdMapRev' M.!))))
         )
     (graphList, nodeIdMapList, nodeIdMapRevList) = unzip3
         [ (neighbors, nodeId, (nodeId, i))
@@ -47,16 +57,16 @@ makeNodeGraphOurs nodeMap =
     nodeIdMap' = A.listArray bounds nodeIdMapList
     nodeIdMapRev' = M.fromList nodeIdMapRevList
 
-makeNodeGraphTheirs :: Map NodeAddr Node -> NodeGraph
-makeNodeGraphTheirs nodeMap =
+makeNodeGraphWithTheirs ::  (a -> Node) -> Map NodeAddr a -> NodeGraph
+makeNodeGraphWithTheirs f nodeMap =
     NodeGraph
         { graph
         , nodeIdMap = view _2 . nodeIdMap'
         , nodeIdMapRev
         }
   where
-    (graph, nodeIdMap', nodeIdMapRev) = graphFromEdges
+    (graph, nodeIdMap', nodeIdMapRev) = G.graphFromEdges
         ( ((), Ret, [])
         : ((), Err, [])
-        : (M.assocs nodeMap <&> \(addr, node) -> ((), Addr addr, toListOf nodeConts node))
+        : (M.assocs nodeMap <&> \(addr, node) -> ((), Addr addr, toListOf nodeConts (f node)))
         )
