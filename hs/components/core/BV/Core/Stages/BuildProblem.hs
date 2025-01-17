825971ff664a845cb69437b6b710f0be297aaea6
Nick Spinale <nick@nickspinale.com>
2025-01-16 16:47:47 -0800

    x

--- hs/components/core/BV/Core/Stages/BuildProblem.hs
+++ hs/components/core/BV/Core/Stages/BuildProblem.hs
@@ -2,7 +2,24 @@ module BV.Core.Stages.BuildProblem
     ( buildProblem
     ) where
 
+import Control.DeepSeq (NFData)
+import Optics
+import qualified Data.Map as M
+import GHC.Generics (Generic)
+
 import BV.Core.Types
 
 buildProblem :: (Tag -> Ident -> Function) -> InlineScript -> PairingOf (Named Function) -> Problem
 buildProblem = undefined
+
+data ProblemBuilder = ProblemBuilder
+    { sides :: PairingOf ProblemSide
+    , nodes :: M.Map NodeAddr NodeWithMeta
+    }
+  deriving (Eq, Generic, NFData, Ord, Show)
+
+data NodeMapBuilder = ProblemBuilder
+    { sides :: PairingOf ProblemSide
+    , nodes :: M.Map NodeAddr NodeWithMeta
+    }
+  deriving (Eq, Generic, NFData, Ord, Show)
