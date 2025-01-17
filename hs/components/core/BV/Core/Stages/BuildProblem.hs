42d64dc020e9dc926e9a40ef4d5c9b81463cbdef
Nick Spinale <nick@nickspinale.com>
2025-01-16 16:50:32 -0800

    x

--- hs/components/core/BV/Core/Stages/BuildProblem.hs
+++ hs/components/core/BV/Core/Stages/BuildProblem.hs
@@ -2,24 +2,41 @@ module BV.Core.Stages.BuildProblem
     ( buildProblem
     ) where
 
-import Control.DeepSeq (NFData)
-import Optics
 import qualified Data.Map as M
+import qualified Data.Set as S
 import GHC.Generics (Generic)
+import Optics
 
 import BV.Core.Types
+import BV.Core.Types (Node, NodeBySource)
 
 buildProblem :: (Tag -> Ident -> Function) -> InlineScript -> PairingOf (Named Function) -> Problem
 buildProblem = undefined
 
-data ProblemBuilder = ProblemBuilder
-    { sides :: PairingOf ProblemSide
-    , nodes :: M.Map NodeAddr NodeWithMeta
-    }
-  deriving (Eq, Generic, NFData, Ord, Show)
+data ProblemBuilder
+  = ProblemBuilder
+      { sides :: PairingOf ProblemSide
+      , nodeMapBuilder :: NodeMapBuilder
+      }
+  deriving (Eq, Generic, Ord, Show)
 
-data NodeMapBuilder = ProblemBuilder
-    { sides :: PairingOf ProblemSide
-    , nodes :: M.Map NodeAddr NodeWithMeta
-    }
-  deriving (Eq, Generic, NFData, Ord, Show)
+data NodeMapBuilder
+  = NodeMapBuilder
+      { nodes :: M.Map NodeAddr NodeWithMeta
+      , nodesBySource :: M.Map NodeSource [NodeAddr]
+      , vars :: S.Set Ident
+      }
+  deriving (Eq, Generic, Ord, Show)
+
+data NodeWithMeta
+  = NodeWithMeta
+      { node :: Node
+      , meta :: NodeMeta
+      }
+  deriving (Eq, Generic, Ord, Show)
+
+data NodeMeta
+  = NodeMeta
+      { bySource :: Maybe NodeBySource
+      }
+  deriving (Eq, Generic, Ord, Show)
