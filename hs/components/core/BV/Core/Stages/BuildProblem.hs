c619643712396fc36154df9892e2e7c0bdab30b2
Nick Spinale <nick@nickspinale.com>
2025-01-17 07:17:55 -0800

    x

--- hs/components/core/BV/Core/Stages/BuildProblem.hs
+++ hs/components/core/BV/Core/Stages/BuildProblem.hs
@@ -1,3 +1,5 @@
+{-# OPTIONS_GHC -Wno-type-defaults #-}
+
 module BV.Core.Stages.BuildProblem
     ( buildProblem
     ) where
@@ -12,6 +14,7 @@ import Optics
 
 import BV.Core.Types
 import BV.Core.Utils
+import Control.Exception (assert)
 
 buildProblem :: (Tag -> Ident -> Function) -> InlineScript -> PairingOf (Named Function) -> Problem
 buildProblem = undefined
@@ -69,16 +72,24 @@ chooseFreshName :: S.Set Ident -> Ident -> Ident
 chooseFreshName taken n =
     if n `S.notMember` taken
     then n
-    else flip evalState (1 :: Integer, 1 :: Integer) $ do
-        let isTaken = (`S.member` taken)
-        let mx = _1
-        let my = _2
-        let fmt x = Ident (n.unwrap ++ "." ++ show x)
-        whileM (use mx <&> \x -> isTaken (fmt x)) $ do
-            x <- use mx
-            assign my x
-            assign mx (x * 2)
-        undefined
+    else loop1 1 1
+  where
+    isTaken = (`S.member` taken)
+    fmt x = Ident (n.unwrap ++ "." ++ show x)
+    loop1 x y =
+        if isTaken (fmt x)
+        then loop1 y (x * 2)
+        else loop2 x y
+    loop2 x y =
+        if x < y
+        then
+            let z = (y + x) `div` 2
+             in if isTaken (fmt z)
+                then loop2 x (z + 1)
+                else loop2 z y
+        else
+            let n' = fmt x
+             in assert (not (isTaken n')) n'
 
 getFreshName :: Ident -> State NodeMapBuilder Ident
 getFreshName hint = do
