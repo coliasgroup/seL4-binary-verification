ed39954a772fd4d1c2c8f28fac6143fe08ff32a4
Nick Spinale <nick@nickspinale.com>
2025-01-30 03:20:02 -0800

    Fix indentation

--- hs/components/system/BV/System/IntermediateArtifacts.hs
+++ hs/components/system/BV/System/IntermediateArtifacts.hs
@@ -53,32 +53,32 @@ runRegisterIntermediateArtifactsT
     => RegisterIntermediateArtifactsT m a
     -> RegisterIntermediateArtifactsTInner m a
 runRegisterIntermediateArtifactsT = iterT $ \(RegisterIntermediateArtifact artifact m) -> do
-        ctx <- ask
-        let register f file actual = do
-                whenJust ctx.dumpTargetDir $ \dumpTargetDir -> do
-                    logInfoN $ "Dumping " <> T.pack file.relativePath
-                    liftIO $ writeTargetDirFile dumpTargetDir file actual
-                whenJust ctx.referenceTargetDir $ \referenceTargetDir -> do
-                    expected <- liftIO $ readTargetDirFile referenceTargetDir file
-                    let actual' = f expected actual
-                    when (force actual' /= force expected) $ do
-                        logErrorN $ "Intermediate artifact mismatch for " <> T.pack file.relativePath
-                        whenJust ctx.mismatchDumpDir $ \mismatchDumpDir -> do
-                            let d = mismatchDumpDir </> file.relativePath
-                            logErrorN $ "Writing mismatch to " <> T.pack d
-                            liftIO $ do
-                                createDirectoryIfMissing True d
-                                writeBVFile (d </> "actual.txt") actual'
-                                writeBVFile (d </> "expected.txt") expected
-                        fail "Intermediate artifact mismatch"
-        case applyWhen ctx.force force artifact of
-            IntermediateArtifactFunctions a -> register noop targetDirFiles.functions a
-            IntermediateArtifactPairings a -> register noop targetDirFiles.pairings a
-            IntermediateArtifactProblems a -> register filterProblems targetDirFiles.problems a
-            IntermediateArtifactFlattenedProofChecks a -> register noop targetDirFiles.proofChecks a
-            IntermediateArtifactFlattenedSMTProofChecks a -> register noop targetDirFiles.smtProofChecks a
-            -- IntermediateArtifactFlattenedSMTProofChecks a -> return ()
-        m
+    ctx <- ask
+    let register f file actual = do
+            whenJust ctx.dumpTargetDir $ \dumpTargetDir -> do
+                logInfoN $ "Dumping " <> T.pack file.relativePath
+                liftIO $ writeTargetDirFile dumpTargetDir file actual
+            whenJust ctx.referenceTargetDir $ \referenceTargetDir -> do
+                expected <- liftIO $ readTargetDirFile referenceTargetDir file
+                let actual' = f expected actual
+                when (force actual' /= force expected) $ do
+                    logErrorN $ "Intermediate artifact mismatch for " <> T.pack file.relativePath
+                    whenJust ctx.mismatchDumpDir $ \mismatchDumpDir -> do
+                        let d = mismatchDumpDir </> file.relativePath
+                        logErrorN $ "Writing mismatch to " <> T.pack d
+                        liftIO $ do
+                            createDirectoryIfMissing True d
+                            writeBVFile (d </> "actual.txt") actual'
+                            writeBVFile (d </> "expected.txt") expected
+                    fail "Intermediate artifact mismatch"
+    case applyWhen ctx.force force artifact of
+        IntermediateArtifactFunctions a -> register noop targetDirFiles.functions a
+        IntermediateArtifactPairings a -> register noop targetDirFiles.pairings a
+        IntermediateArtifactProblems a -> register filterProblems targetDirFiles.problems a
+        IntermediateArtifactFlattenedProofChecks a -> register noop targetDirFiles.proofChecks a
+        IntermediateArtifactFlattenedSMTProofChecks a -> register noop targetDirFiles.smtProofChecks a
+        -- IntermediateArtifactFlattenedSMTProofChecks a -> return ()
+    m
   where
     filterProblems expected actual = actual & #unwrap %~ M.filterWithKey (\k _v -> k `M.member` expected.unwrap)
     noop _expected actual = actual
