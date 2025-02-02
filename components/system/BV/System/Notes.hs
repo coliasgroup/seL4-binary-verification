a1daca6b5d772c91fd68f8146a844dad7f0f331f
Nick Spinale <nick@nickspinale.com>
2025-02-02 01:37:01 -0800

    Organize notes

--- components/system/BV/System/Notes.hs
+++ components/system/BV/System/Notes.hs
@@ -22,3 +22,21 @@ import GHC.Generics (Generic)
 --   = TaskSMTProofCheckGroup (Task (SMTProofCheckGroup (SMTProofCheckDescription String)) (SMTProofCheckResult ()))
 --   | TaskProofSearch (Task PairingId ProofSearchResult)
 --   deriving (Eq, Generic)
+
+--
+
+-- data ProblemCheckError
+--   = NoSolversAnswered
+--   | SomeSolverAnsweredSat
+--   | AllSolversAnsweredUnknown
+--   deriving (Eq, Generic, Ord, Show)
+
+-- data AcceptableSatResult
+--   = AcceptableSatResultSat
+--   | AcceptableSatResultUnsat
+--   deriving (Eq, Generic, Ord, Show)
+
+-- class Monad m => MonadCache n m | m -> n where
+--     checkSMTProofCheckWithCache :: Units -> CacheWrapper n m (SMTProofCheckGroup ()) AcceptableSatResult
+
+-- type CacheWrapper n m a b = (a -> n b) -> a -> m b
