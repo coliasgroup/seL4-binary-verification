0f4db2afb3aee086a9abbb2a49d9f480c5efbf50
Nick Spinale <nick@nickspinale.com>
2025-02-04 01:01:59 -0800

    Clean up logic ExecuteSMTProofChecks

--- components/core/BV/Core/ExecuteSMTProofChecks.hs
+++ components/core/BV/Core/ExecuteSMTProofChecks.hs
@@ -21,10 +21,8 @@ import Control.Monad (forM_)
 import Control.Monad.Catch (MonadThrow)
 import Control.Monad.Error.Class (throwError)
 import Control.Monad.Except (runExceptT)
-import Control.Monad.State (StateT, evalStateT)
+import Control.Monad.State (evalStateT)
 import Control.Monad.State.Class (MonadState)
-import Control.Monad.Trans.Class (lift)
-import Control.Monad.Trans.Except (ExceptT (ExceptT))
 import Control.Monad.Trans.Writer (runWriterT)
 import Control.Monad.Writer (tell)
 import Data.Function (applyWhen)
@@ -33,6 +31,9 @@ import Data.Tuple (swap)
 import GHC.Generics (Generic)
 import Optics
 import Optics.State.Operators ((<<%=))
+
+logic :: String
+logic = "QF_AUFBV"
 
 executeSMTProofCheckOffline
     :: MonadSolver m
@@ -58,52 +59,41 @@ executeSMTProofCheckGroupOnline
     -> m (Either (a, OnlineSolverAbortReason) (), [a])
 executeSMTProofCheckGroupOnline config timeout group = do
     sendSimpleCommandExpectingSuccess $ SetOption (PrintSuccessOption True)
-    sendSimpleCommandExpectingSuccess $ SetLogic "QF_AUFBV"
+    sendSimpleCommandExpectingSuccess $ SetLogic logic
     mapM_ sendExpectingSuccess (smtConfigPreamble config)
     mapM_ (sendExpectingSuccess . configureSExpr config) group.setup
-    (abortInfo, completed) <- flip evalStateT 1 . runWriterT . runExceptT . forM_ group.imps $ \check -> do
-        let meta = check.meta
-        let hyp = check.term
-        let hyp' = notS hyp
-        let split = splitHyp hyp'
-        lift . sendSimpleCommandExpectingSuccess $ Push 1
-        forM_ split $ \hyp -> do
-            labeledHyp <- labelSExpr hyp
-            lift . sendSimpleCommandExpectingSuccess . Assert . Assertion . configureSExpr config $
-                labeledHyp
-        result <- ExceptT $ maybe (Left (meta, OnlineSolverAbortReasonTimeout)) Right <$> checkSatWithTimeout timeout
-        case result of
-            Sat -> throwError (meta, OnlineSolverAbortReasonAnsweredSat)
-            Unknown -> throwError (meta, OnlineSolverAbortReasonAnsweredUnknown)
-            Unsat -> return ()
-        tell [meta]
-        lift . sendSimpleCommandExpectingSuccess $ Pop 1
-        lift . sendSimpleCommandExpectingSuccess . Assert . Assertion . configureSExpr config $
-            notS (andNS split)
+    (abortInfo, completed) <-
+        flip evalStateT 1 {- matches graph-refine -} .
+        runWriterT .
+        runExceptT .
+            forM_ group.imps $ \check -> do
+                let meta = check.meta
+                let hyps = splitHyp (notS check.term)
+                sendSimpleCommandExpectingSuccess $ Push 1
+                forM_ hyps $ \hyp -> do
+                    labeledHyp <- labelHyp hyp
+                    sendAssert labeledHyp
+                result <- checkSatWithTimeout timeout >>=
+                    maybe (throwError (meta, OnlineSolverAbortReasonTimeout)) return
+                case result of
+                    Sat -> throwError (meta, OnlineSolverAbortReasonAnsweredSat)
+                    Unknown -> throwError (meta, OnlineSolverAbortReasonAnsweredUnknown)
+                    Unsat -> return ()
+                tell [meta]
+                sendSimpleCommandExpectingSuccess $ Pop 1
+                sendAssert $ notS (andNS hyps)
     return (abortInfo, completed)
+  where
+    sendAssert = sendSimpleCommandExpectingSuccess . Assert . Assertion . configureSExpr config
 
-labelSExpr :: MonadState Integer m => SExprWithPlaceholders -> m SExprWithPlaceholders
-labelSExpr sexpr = do
+labelHyp :: MonadState Integer m => SExprWithPlaceholders -> m SExprWithPlaceholders
+labelHyp sexpr = do
     i <- simple <<%= (+ 1)
     let label = "hyp" ++ show i
     return $ labelS label sexpr
 
 splitHyp :: SExprWithPlaceholders -> [SExprWithPlaceholders]
-splitHyp = splitHypSExpr
---     if split
---     then splitHypSExpr hyp
---     else [hyp]
---   where
---     hyp = imp.term
---     split = any (`matchPatternS` hyp)
---         [ ["and"]
---         , ["not", ["=>"]]
---         , ["not", ["or"]]
---         , ["not", ["not"]]
---         ]
-
-splitHypSExpr :: SExprWithPlaceholders -> [SExprWithPlaceholders]
-splitHypSExpr = fromJust . traverse checkSExprWithPlaceholders . go . viewSExprWithPlaceholders
+splitHyp = fromJust . traverse checkSExprWithPlaceholders . go . viewSExprWithPlaceholders
   where
     notU x = ["not", x]
     isBool = (`elem` (["true", "false"] :: [_]))
