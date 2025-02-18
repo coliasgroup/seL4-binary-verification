1f7ed961ca67f5aca6242f7e01e50f52e4703533
Nick Spinale <nick@nickspinale.com>
2025-02-17 17:12:41 -0800

    Add skeleton to distrib-scratch

--- components/scratch/distrib-scratch/Main.hs
+++ components/scratch/distrib-scratch/Main.hs
@@ -2,5 +2,22 @@ module Main
     ( main
     ) where
 
+
+import Opts
+
 main :: IO ()
-main = return ()
+main = do
+    opts <- parseOpts
+    case opts of
+        CommandDriver opts' -> runDriver opts'
+        CommandWorker opts' -> runWorker opts'
+
+runDriver :: DriverOpts -> IO ()
+runDriver opts = do
+    putStrLn "driver"
+    return ()
+
+runWorker :: WorkerOpts -> IO ()
+runWorker opts = do
+    putStrLn "worker"
+    return ()
---
+++ components/scratch/distrib-scratch/Opts.hs
@@ -0,0 +1,53 @@
+{-# LANGUAGE ApplicativeDo #-}
+
+module Opts
+    ( Opts (..)
+    , WorkerOpts (..)
+    , parseOpts
+    , DriverOpts (..)
+    ) where
+
+import Options.Applicative
+
+import GHC.Generics (Generic)
+
+data Opts = CommandDriver DriverOpts | CommandWorker WorkerOpts
+  deriving (Generic, Show)
+
+data DriverOpts
+  = DriverOpts
+      {
+      }
+  deriving (Generic, Show)
+
+data WorkerOpts
+  = WorkerOpts
+      {
+      }
+  deriving (Generic, Show)
+
+parseOpts :: IO Opts
+parseOpts = customExecParser
+    (prefs (subparserInline <> helpShowGlobals))
+    optsParserInfo
+
+optsParserInfo :: ParserInfo Opts
+optsParserInfo =
+    info (optsParser <**> helper) mempty
+
+optsParser :: Parser Opts
+optsParser =
+    subparser (mconcat
+        [ command "driver"
+            (info (CommandDriver <$> parseDriverOpts) mempty)
+        , command "worker"
+            (info (CommandWorker <$> parseWorkerrOpts) mempty)
+        ])
+
+parseDriverOpts :: Parser DriverOpts
+parseDriverOpts = do
+    pure DriverOpts
+
+parseWorkerrOpts :: Parser WorkerOpts
+parseWorkerrOpts = do
+    pure WorkerOpts
