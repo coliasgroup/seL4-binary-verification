8231c6f75078c795555d0d6188d2cbb1f8b6574d
Nick Spinale <nick@nickspinale.com>
2025-02-17 17:27:36 -0800

    Set up simplelocalnet, not working

--- components/scratch/distrib-scratch/Main.hs
+++ components/scratch/distrib-scratch/Main.hs
@@ -2,8 +2,12 @@ module Main
     ( main
     ) where
 
-
 import Opts
+
+import Control.Distributed.Process
+import Control.Distributed.Process.Node (initRemoteTable, runProcess)
+import Control.Distributed.Process.Backend.SimpleLocalnet
+import Control.Monad (forever, forM_)
 
 main :: IO ()
 main = do
@@ -14,10 +18,23 @@ main = do
 
 runDriver :: DriverOpts -> IO ()
 runDriver opts = do
-    putStrLn "driver"
-    return ()
+    let host = "localhost"
+    let port = "9001"
+    putStrLn "run driver"
+    backend <- initializeBackend host port initRemoteTable
+    startMaster backend (master backend)
 
 runWorker :: WorkerOpts -> IO ()
 runWorker opts = do
-    putStrLn "worker"
-    return ()
+    let host = "localhost"
+    let port = "9002"
+    putStrLn "run worker"
+    backend <- initializeBackend host port initRemoteTable
+    startSlave backend
+
+master :: Backend -> [NodeId] -> Process ()
+master backend slaves = do
+    -- Do something interesting with the slaves
+    liftIO . putStrLn $ "Slaves: " ++ show slaves
+    -- Terminate the slaves when the master terminates (this is optional)
+    terminateAllSlaves backend
