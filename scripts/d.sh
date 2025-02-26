06c029877c229736bea767e47bf8a6048c3f1d7a
Nick Spinale <nick@nickspinale.com>
2025-02-26 07:14:00 -0800

    Rename distrib-scratch -> remote-scratch
    To work around overeager Nixpkgs Haskell source filter

--- scripts/d.sh
+++ scripts/d.sh
@@ -9,8 +9,8 @@ here=$(dirname $0)
 # env="DISTRIBUTED_PROCESS_TRACE_CONSOLE=1 DISTRIBUTED_PROCESS_TRACE_FLAGS=pdnusrl"
 env=""
 
-# env $env cabal run distrib-scratch -- worker &
+# env $env cabal run remote-scratch -- worker &
 # sleep 3
-# env $env cabal run distrib-scratch -- driver
+# env $env cabal run remote-scratch -- driver
 
-env $env cabal run distrib-scratch -- driver
+env $env cabal run remote-scratch -- driver
