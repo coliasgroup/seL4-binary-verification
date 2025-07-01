880829e3dd0ec35a65c112f9be9fe3dafac6d91c
Nick Spinale <nick@nickspinale.com>
2025-07-01 01:26:39 -0700

    Default to small tests

--- components/tests/core-test/Main.hs
+++ components/tests/core-test/Main.hs
@@ -33,8 +33,8 @@ testStages = do
     output.intermediate.compatSMTProofChecks `deepseq` return ()
   where
     referenceTargetDir =
-        testSeL4TargetDirBig
-        -- testSeL4TargetDirSmall
+        -- testSeL4TargetDirBig
+        testSeL4TargetDirSmall
         -- testSeL4TargetDirFocused
 
 testStagesWithChecking :: IO ()
@@ -50,6 +50,6 @@ testStagesWithChecking = do
         , mismatchDumpDir = Just $ tmpDir </> "mismatch"
         }
     referenceTargetDir =
-        testSeL4TargetDirBig
-        -- testSeL4TargetDirSmall
+        -- testSeL4TargetDirBig
+        testSeL4TargetDirSmall
         -- testSeL4TargetDirFocused
