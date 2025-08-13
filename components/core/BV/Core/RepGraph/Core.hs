1f5073b7460e30f94f2b683410f4dd6459f320fa
Nick Spinale <nick@nickspinale.com>
2025-08-13 15:28:32 -0700

    x

--- components/core/BV/Core/RepGraph/Core.hs
+++ components/core/BV/Core/RepGraph/Core.hs
@@ -640,9 +640,6 @@ getLoopPcEnv visit = do
         memCalls <- scanMemCallsEnv prevEnv >>= addLoopMemCalls visitAddr
         nonConsts <- filterM (fmap not . isConstM) (M.keys prevEnv)
         let mkName ident = printf "%s_after_loop_at_%s" ident.unwrap (prettyNodeId visit.nodeId)
-        -- let add name ty = do
-        --         let hint = printf "%s_loop_at_%s" name (prettyNodeId visit.nodeId)
-        --         addVarRestrWithMemCalls hint ty memCalls
         env <- xxx
             mkName
             memCalls
@@ -650,15 +647,6 @@ getLoopPcEnv visit = do
             visit
             nonConsts
             prevEnv
-        -- notSplitEnv <- flip M.traverseWithKey prevEnv $ \var v ->
-        --     if isConst var
-        --     then return v
-        --     else NotSplit . nameS <$> add (var.name.unwrap ++ "_after") var.ty
-        -- env <- flip M.traverseWithKey notSplitEnv $ \var v ->
-        --     if isConst var
-        --     then return v
-        --     else maybe v Split <$> varRepRequest var VarRepRequestKindLoop visit notSplitEnv
-        -- pc <- smtExprE boolT . NotSplit . nameS <$> add "pc_of" boolT
         pc <- smtExprE boolT <$>
             addVarRestrWithMemCalls
                 (printf "pc_of_loop_at_%s" (prettyNodeId visit.nodeId))
