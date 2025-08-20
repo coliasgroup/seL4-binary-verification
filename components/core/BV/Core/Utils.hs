6247cb79395cda4696f3710322429b7f07f4e543
Nick Spinale <nick@nickspinale.com>
2025-08-20 02:01:10 -0700

    Generalize zipWithTraversable

--- components/core/BV/Core/Utils.hs
+++ components/core/BV/Core/Utils.hs
@@ -3,12 +3,32 @@ module BV.Core.Utils
     , whenJustThen
     , whenNothing
     , zipWithTraversable
+    , zipWithTraversableM
     ) where
 
-import Control.Monad.State (evalState, state)
+import Control.Monad.Identity (Identity (Identity, runIdentity))
+import Control.Monad.State (evalStateT, state)
+import Control.Monad.Trans (lift)
 import Control.Monad.Trans.Maybe (MaybeT (MaybeT, runMaybeT), hoistMaybe)
+import Data.Foldable (toList)
 import Data.Maybe (fromJust)
 import Optics
+
+whenNothing :: Monad m => Maybe a -> m a -> m a
+whenNothing opt m = maybe m return opt
+
+whenJustThen :: Monad m => Maybe a -> (a -> m (Maybe b)) -> m (Maybe b)
+whenJustThen opt f = runMaybeT $ hoistMaybe opt >>= MaybeT . f
+
+zipWithTraversableM :: (Foldable t1, Traversable t2, Monad m) => (a -> b -> m c) -> t1 a -> t2 b -> m (t2 c)
+zipWithTraversableM f t1 t2 = evalStateT (traverse m t2) (toList t1)
+  where
+    m b = do
+        a <- state $ fromJust . uncons
+        lift $ f a b
+
+zipWithTraversable :: (Foldable t1, Traversable t2) => (a -> b -> c) -> t1 a -> t2 b -> t2 c
+zipWithTraversable f t1 t2 = runIdentity $ zipWithTraversableM (\a b -> Identity (f a b)) t1 t2
 
 liftIso :: Iso' c (a, b) -> Lens' s a -> Lens' s b -> Lens' s c
 liftIso f l r =
@@ -21,19 +41,3 @@ liftIso f l r =
 
 adjacently :: Lens' s a -> Lens' s b -> Lens' s (a, b)
 adjacently = liftIso simple
-
-whenNothing :: Monad m => Maybe a -> m a -> m a
-whenNothing opt m = maybe m return opt
-
-whenJustThen :: Monad m => Maybe a -> (a -> m (Maybe b)) -> m (Maybe b)
-whenJustThen opt f = runMaybeT $ hoistMaybe opt >>= MaybeT . f
-
--- (!) :: (HasCallStack, Show k, Ord k) => M.Map k a -> k -> a
--- (!) = findWithCallstack
-
-zipWithTraversable :: Traversable t => (a -> b -> c) -> [a] -> t b -> t c
-zipWithTraversable f xs t = evalState (traverse m t) xs
-  where
-    m b = do
-        a <- state $ fromJust . uncons
-        return $ f a b
