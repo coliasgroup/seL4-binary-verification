5f64e358addbb9bd306e03ed5aba4b4de4004558
Nick Spinale <nick@nickspinale.com>
2025-09-12 17:40:13 -0700

    Clean up rep graph choice module

--- components/core/BV/Core/RepGraph.hs
+++ components/core/BV/Core/RepGraph.hs
@@ -1,10 +1,10 @@
 module BV.Core.RepGraph
-    ( XXX
-    , module BV.Core.RepGraph.Old
-    -- , module BV.Core.RepGraph.New
+    ( module BV.Core.RepGraph.Old
     ) where
+
+-- module BV.Core.RepGraph
+--     ( module BV.Core.RepGraph.New
+--     ) where
 
 import BV.Core.RepGraph.Old
 -- import BV.Core.RepGraph.New
-
-type XXX = ()
