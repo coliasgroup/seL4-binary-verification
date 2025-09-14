14bdc5ff422d2dc515041c7594e3661b9c5e8b99
Nick Spinale <nick@nickspinale.com>
2025-09-13 18:37:45 -0700

    x

---
+++ components/core/BV/Core/RepGraph/New/ExprCommand.hs
@@ -0,0 +1,27 @@
+{-# LANGUAGE DeriveAnyClass #-}
+
+module BV.Core.RepGraph.New.ExprCommand
+    ( ExprCommand (..)
+    , ExprCommandInlineHint (..)
+    ) where
+
+import BV.Core.Types (Expr, NameTy)
+
+import Control.DeepSeq (NFData)
+import Data.Binary (Binary)
+import GHC.Generics (Generic)
+
+data ExprCommand c
+  = ExprCommandDeclare NameTy
+  | ExprCommandDefine ExprCommandInlineHint NameTy (Expr c)
+  | ExprCommandAssert (Expr c)
+  deriving (Eq, Generic, NFData, Ord, Show)
+
+instance Binary (ExprCommand c)
+
+data ExprCommandInlineHint
+  = ExprCommandInlineHintNever
+  | ExprCommandInlineHintAlways
+  deriving (Eq, Generic, NFData, Ord, Show)
+
+instance Binary ExprCommandInlineHint
