d4ae184de94bc386251483b0ffc1a3e978011064
Nick Spinale <nick@nickspinale.com>
2025-10-04 04:58:08 -0700

    XXX switch to new graph slice in search

--- components/search/search-core/BV/Search/Core/GraphSlice.hs
+++ components/search/search-core/BV/Search/Core/GraphSlice.hs
@@ -1,10 +1,10 @@
+-- module BV.Search.Core.GraphSlice
+--     ( module BV.Core.GraphSlice.Old
+--     ) where
+
 module BV.Search.Core.GraphSlice
-    ( module BV.Core.GraphSlice.Old
+    ( module BV.Core.GraphSlice.New
     ) where
 
--- module BV.Search.Core.GraphSlice
-    -- ( module BV.Core.GraphSlice.New
-    -- ) where
-
-import BV.Core.GraphSlice.Old
--- import BV.Core.GraphSlice.New
+-- import BV.Core.GraphSlice.Old
+import BV.Core.GraphSlice.New
